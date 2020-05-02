3f4fe2d17570745f04738e7c575307e201577a20
Derek Haynes <derek.haynes@gmail.com>
2020-05-02 08:12:59 -0600

    Working implementation

--- src/models/model_wrapper.py
+++ src/models/model_wrapper.py
@@ -1,13 +1,8 @@
 import pickle
-from nltk.stem import WordNetLemmatizer 
-
-class LemmaTokenizer(object):
-    def __init__(self):
-        self.wnl = WordNetLemmatizer()
-    def __call__(self, articles):
-        return [self.wnl.lemmatize(t) for t in word_tokenize(articles)]
-
-LemmaTokenizer = LemmaTokenizer()
+from nltk.stem import WordNetLemmatizer
+import os
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
 class ModelWrapper:
     """
@@ -31,48 +26,53 @@ class ModelWrapper:
             with open('models/tokenizer.pickle', 'rb') as handle:
                 self.tokenizer = pickle.load(handle)
         """
-        with open('models/repo_lookup.pickle', 'rb') as handle:
+
+        # Get path to the models directory based on this file's path. We do this relative to this file
+        # vs. just "models/" as "models/" assumes the current working directory is the top-level directory.
+        # That might not be the caes (for example, when using this class within a notebook contained in notebooks/).
+        file_dir = os.path.dirname(__file__)
+        models_dir = os.path.join(file_dir, "../../models/")
+
+        with open(models_dir+'repo_lookup.pickle', 'rb') as handle:
                 self.repo_lookup = pickle.load(handle)
 
-        with open('models/sparse_vector_matrix.pickle', 'rb') as handle:
+        with open(models_dir+'sparse_vector_matrix.pickle', 'rb') as handle:
                 self.sparse_vector_matrix = pickle.load(handle)
 
-        with open('models/word_vectorizer.pickle', 'rb') as handle:
+        with open(models_dir+'word_vectorizer.pickle', 'rb') as handle:
                 self.word_vectorizer = pickle.load(handle)
 
-    
-    def text_recommender(self
-                        , input_df
-                        , word_vectorizer
-                        , sparse_vector_matrix
-                        , repo_lookup):
-        
+
+    def text_recommender(self, input_df):
+
         input_df['bag_of_words'] = input_df.apply(lambda x: ' '.join(x), axis = 1)
-        
+
         # vectorize the inputted string
         #inputted_vector = word_vectorizer.transform(pd.Series(str(input_string)))
-        inputted_vector = word_vectorizer.transform(input_df['bag_of_words'])
-        
+        inputted_vector = self.word_vectorizer.transform(input_df['bag_of_words'])
+
         # calculate cosine similarity with existing matrix
-        one_dimension_cosine_sim = cosine_similarity(inputted_vector, sparse_vector_matrix)
-    
+        one_dimension_cosine_sim = cosine_similarity(inputted_vector, self.sparse_vector_matrix)
+
         # creating a Series with the similarity scores in descending order
         score_series = pd.Series(one_dimension_cosine_sim[0]).sort_values(ascending = False)
         # only show matches that have some similarity
         score_series = score_series[score_series>0]
-    
+
         # getting the indexes of the 10 most similar repos
         top_10_indexes = list(score_series.iloc[1:11].index)
-        
+
         # initializing the empty list of recommended repo
-        
-        recommended_repos = repo_lookup.loc[top_10_indexes]
-            
+
+        recommended_repos = self.repo_lookup.loc[top_10_indexes]
+
         return recommended_repos
 
     def predict(self,data):
         """
         Returns model predictions.
+        Data is a list of space-seperated strings.
+        Example: ["python dashboard"]
         """
-        # Add any required pre/post-processing steps here.
-        return self.text_recommender(data)
+        df = pd.DataFrame.from_records(data)
+        return self.text_recommender(df)
